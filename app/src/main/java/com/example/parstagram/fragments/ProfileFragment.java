285a91f1de0c08dbcc950b6f135ff3159dc77342
Anna Zhang <annayzhang@fb.com>
2020-07-08 14:19:32 -0700

    User can take a profile picture from the app

--- app/src/main/java/com/example/parstagram/fragments/ProfileFragment.java
+++ app/src/main/java/com/example/parstagram/fragments/ProfileFragment.java
@@ -1,13 +1,20 @@
 package com.example.parstagram.fragments;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,20 +22,27 @@ import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+import com.example.parstagram.MainActivity;
 import com.example.parstagram.OpeningActivity;
 import com.example.parstagram.Post;
 import com.example.parstagram.R;
+import com.parse.ParseException;
 import com.parse.ParseFile;
 import com.parse.ParseUser;
+import com.parse.SaveCallback;
+
+import java.io.File;
+
+import static android.app.Activity.RESULT_OK;
 
 public class ProfileFragment extends Fragment {
-
+    private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 43;
     private static final String TAG = "ProfileFragment";
 
     ParseUser user;
-
     ImageView ivProfile;
     TextView tvName;
     TextView tvUsername;
@@ -36,6 +50,8 @@ public class ProfileFragment extends Fragment {
     Button btnEdit;
     Button btnLogout;
     public static FragmentManager fragmentManager;
+    File photoFile;
+    String photoFileName = "photo.jpg";
 
     public ProfileFragment() {
         // Required empty public constructor
@@ -95,10 +111,86 @@ public class ProfileFragment extends Fragment {
                 goOpening();
             }
         });
+        btnEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                launchCamera();
+            }
+        });
     }
 
     private void goOpening() {
         Intent intent = new Intent(getContext(), OpeningActivity.class);
         startActivity(intent);
+    }
+
+    public void launchCamera() {
+        // create Intent to take a picture and return control to the calling application
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Create a File reference for future access
+        photoFile = getPhotoFileUri(photoFileName);
+
+        // wrap File object into a content provider
+        // required for API >= 24
+        // See https://guides.codepath.com/android/Sharing-Content-with-Intents#sharing-files-with-api-24-or-higher
+        Uri fileProvider = FileProvider.getUriForFile(getContext(), "com.parstagram.fileprovider", photoFile);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider);
+
+        // If you call startActivityForResult() using an intent that no app can handle, your app will crash.
+        // So as long as the result is not null, it's safe to use the intent.
+        if (intent.resolveActivity(getContext().getPackageManager()) != null) {
+            // Start the image capture intent to take photo
+            startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                final Bitmap takenImage = BitmapFactory.decodeFile(photoFile.getAbsolutePath());
+                // TODO resize the bitmap
+//                ivProfile.setImageBitmap(takenImage);
+                user.put(Post.KEY_PROFILEPIC, new ParseFile(photoFile));
+                user.saveInBackground(new SaveCallback() {
+                    @Override
+                    public void done(ParseException e) {
+                        if (e != null) {
+                            Log.e(TAG, "Issue saving the profile picure" , e);
+                            Toast.makeText(getContext(), "Error while saving!", Toast.LENGTH_LONG).show();
+                            return;
+                        }
+
+                        Log.i(TAG, "User image was saved!!");
+//                        etDescription.setText("");
+//                        pd.dismiss();
+                        Glide.with(getContext()).load(takenImage).circleCrop().into(ivProfile);
+
+                    }
+                });
+            } else { // Result was a failure
+                Log.i(TAG, "Picture wasn't taken");
+//                Toast.makeText(getContext(), "Picture wasn't taken!", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    // Returns the File for a photo stored on disk given the fileName
+    public File getPhotoFileUri(String fileName) {
+        // Get safe storage directory for photos
+        // Use `getExternalFilesDir` on Context to access package-specific directories.
+        // This way, we don't need to request external read/write runtime permissions.
+        File mediaStorageDir = new File(getContext().getExternalFilesDir(Environment.DIRECTORY_PICTURES), TAG);
+
+        // Create the storage directory if it does not exist
+        if (!mediaStorageDir.exists() && !mediaStorageDir.mkdirs()){
+            Log.d(TAG, "failed to create directory");
+        }
+
+        // Return the file target for the photo based on filename
+        File file = new File(mediaStorageDir.getPath() + File.separator + fileName);
+
+        return file;
     }
 }
